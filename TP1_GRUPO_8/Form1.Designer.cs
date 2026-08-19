b1b41085ee6cc16fbfef9399b666bd22335db8ed
ProgramadoraDaina <Daina.Paucar@alumnos.frgp.utn.edu.ar>
2026-08-19 11:29:41 -0300

    feat: add main form layout and exercise navigation buttons

--- TP1_GRUPO_8/Form1.Designer.cs
+++ TP1_GRUPO_8/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace TP1_GRUPO_8
+﻿using System;
+
+namespace TP1_GRUPO_8
 {
     partial class Form1
     {
@@ -28,13 +30,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 76);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ejercicio 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(320, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 76);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Ejercicio 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(575, 34);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(169, 76);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Ejercicio 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 25F);
+            this.label1.Location = new System.Drawing.Point(313, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 39);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Integrantes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 15F);
+            this.label2.Location = new System.Drawing.Point(309, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 39);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "-Goitia Dante" + Environment.NewLine +
+                               "-Luque Jonatan" + Environment.NewLine +
+                               "-Daina Paucar" + Environment.NewLine +
+                               "-Pousada Paloma" + Environment.NewLine +
+                               "-Salazar Miguel" + Environment.NewLine +
+                               "-Santillán Candela";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- TP1_GRUPO_8/Form1.cs
+++ TP1_GRUPO_8/Form1.cs
@@ -16,5 +16,22 @@ namespace TP1_GRUPO_8
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
     }
 }
